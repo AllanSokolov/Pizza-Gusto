54efb8adf3ca13c5c5855f609523653297bef9c7
AllanSokolov <sokolov.allan683@gmail.com>
2020-07-10 19:53:03 +0200

    Add social, awards content and footer

--- src/js/myscript.js
+++ src/js/myscript.js
@@ -15,3 +15,24 @@ $(document).ready(function () {
     $('.animated-icon').toggleClass('open');
   });
 });
+
+//Scroll to top button
+
+// When the user scrolls down 200px from the top of the document, the button will show
+window.onscroll = function()
+{
+    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200)
+    {
+    	document.getElementById("toTopBtn").style.display = "block";
+    }
+    else
+    {
+    	document.getElementById("toTopBtn").style.display = "none";
+    }
+};
+
+function scrollToTop()
+{
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox and Opera
+}
